8c8befa474fd87375d087e147e623602fb4e6ae3
serghov <sergey.hovakimyan@gmail.com>
2016-05-04 16:31:35 +0400

    working test

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 {
 	vector<int> shape(2);
 	shape[0] = 1;
-	shape[1] = 1;
+	shape[1] = 20;
 
 
 
@@ -20,27 +20,37 @@ int main()
 
 	
 
-	NeuralNetwork myNet(shape,new linear,new linear,new L1, inputNames);//make static vars for math functions
+	NeuralNetwork myNet(shape,new sigmoid,new sigmoid,new L1, inputNames);//make static vars for math functions
 
 	vector<double> input(1);
-	input[0] = 0.1;
+	input[0] = 20;
 	cout << fixed << setprecision(5);
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 100; i++)
 	{
+		myNet.forwardProp(input);
+		myNet.backwardProp(0.3, new squareError);
+
+
 		cout << "output \t" << myNet.network[1][0].output << endl;
 		cout << "deriv \t" << myNet.network[myNet.network.size() - 1][0].outputDer << endl;
 
 		cout << "weight \t" << myNet.network[1][0].inputs[0]->weight << endl;
 		cout << "bias \t" << myNet.network[1][0].bias << endl;
 
+		cout << "weights" << endl;
+
+		for (int j = 0; j < myNet.network[0][0].outputs.size(); j++)		
+			cout << myNet.network[0][0].outputs[j]->weight << " ";
+		
+		cout << endl;
 		
 		cout << "------------" << endl;
 
-		myNet.forwardProp(input);
-		myNet.backwardProp(1, new squareError);
+		
+		
 
-		myNet.updateWeights(0.1, 0);//no idea why
+		myNet.updateWeights(0.1, 0);
 		
 	}
 	
