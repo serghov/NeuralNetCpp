52e7720702964312adc98df23dd95bc0f7b1eea2
serghov <sergey.hovakimyan@gmail.com>
2016-05-08 14:10:36 +0400

    drawing single variable regression functions

--- main.cpp
+++ main.cpp
@@ -4,54 +4,87 @@
 #include <vector>
 #include <iomanip>
 
-using namespace std;
+
+
+void plotSingleVarNet(NeuralNetwork &myNet, double rangeStart, double rangeEnd)
+{
+	int i, j;
+	vector<double> res(60);
+	for (i = 0; i < 60; i++)
+	{
+		res[i] = myNet.forwardProp(vector<double>{ (i+0.0) / (rangeEnd - rangeStart) / 60.0 + rangeStart});	
+	}
+	
+	for (i = 0; i < 60; i++)
+		cout << "_";
+	cout << endl;
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if ((int)(res[j] * 11) == 10 - i)
+				cout << "+";
+			else
+				cout << " ";
+		}
+		cout <<"| "<<(10-i)/10.0 *(rangeEnd - rangeStart) + rangeStart << endl;
+	}
+	for (i = 0; i < 60; i++)
+		cout << "_";
+	cout << endl;
+}
+
 
 int main()
 {
-	vector<int> shape(2);
+	vector<int> shape(3);
 	shape[0] = 1;
 	shape[1] = 20;
+	shape[2] = 1;
 
 
 
 	vector<string> inputNames(1);
 	inputNames[0] = "uxt";
-	//inputNames[1] = "dzi";
 
-	
+	NeuralNetwork myNet(shape,new sigmoid,new sigmoid,new L1, inputNames);//make static vars for math functions //and a namespace
 
-	NeuralNetwork myNet(shape,new sigmoid,new sigmoid,new L1, inputNames);//make static vars for math functions
-
-	vector<double> input(1);
-	input[0] = 20;
 	cout << fixed << setprecision(5);
 
-	for (int i = 0; i < 100; i++)
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_real_distribution<> rand01(-0.1, 0.1);
+	uniform_real_distribution<> rand001(-0.01, 0.01);
+
+
+	for (int i = 0; i < 10000; i++)
 	{
-		myNet.forwardProp(input);
-		myNet.backwardProp(0.3, new squareError);
-
-
-		cout << "output \t" << myNet.network[1][0].output << endl;
-		cout << "deriv \t" << myNet.network[myNet.network.size() - 1][0].outputDer << endl;
-
-		cout << "weight \t" << myNet.network[1][0].inputs[0]->weight << endl;
-		cout << "bias \t" << myNet.network[1][0].bias << endl;
-
-		cout << "weights" << endl;
-
-		for (int j = 0; j < myNet.network[0][0].outputs.size(); j++)		
-			cout << myNet.network[0][0].outputs[j]->weight << " ";
+		for (int j = 0; j < 50; j++)
+		{
+			myNet.forwardProp(vector<double>{0.5 + rand01(gen)});			
+			myNet.backwardProp(1, new squareError);
+		}
 		
-		cout << endl;
-		
-		cout << "------------" << endl;
+		for (int j = 0; j < 50; j++)
+		{
+			myNet.forwardProp(vector<double>{0 + rand01(gen)});
+			myNet.backwardProp(0, new squareError);
+		}
+
+		if (i % 1000 == 0)
+		{
+			cout << "Epoch: " << i << endl;
+			plotSingleVarNet(myNet, 0, 1);
+			cout << endl << endl;
+		}
 
 		myNet.updateWeights(0.1, 0);
 		
 	}
 	
-	squareError *tmp = new squareError;
+	
+	plotSingleVarNet(myNet, 0, 1);
+
 
 	int t;
 	cin >> t;
