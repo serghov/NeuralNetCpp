551f1c86d9d9ff2c2ef37e172388db958195e650
serghov <sergey.hovakimyan@gmail.com>
2016-05-16 16:06:01 +0400

    better structure for running

--- main.cpp
+++ main.cpp
@@ -39,49 +39,14 @@ void plotSingleVarNet(NeuralNetwork &myNet, double rangeStart, double rangeEnd)
 	cout << endl;
 }
 
-
-int main()
+void doAnealingOnMnist(NeuralNetwork &myNet, MNIST &mnist)
 {
-	MNIST mnist("./train.csv", 42000);
-
-	vector<int> shape{ 784 , 28, 10 };
-
-	NeuralNetwork myNet(shape, Activations::SIGMOID, Activations::SIGMOID, Regularizations::L2, true);
-
-	ofstream fout("network.txt");
-	fout << myNet.toString();
-	fout.close();
-
-	cout << fixed << setprecision(5);
-
-	random_device rd;
-	mt19937 gen(rd());
-
-	uniform_real_distribution<> rand001(-0.01, 0.01);
-	uniform_real_distribution<> rand01(0, 1);
-
-
-	double alpha = 0.1;
-	//29400 levon, 12600
-
-	int curIterationNum = 0;
-
-	double lastPercentage = 0;
-
-	vector<vector<double> > trainData(29400);
-	vector<int> trainLabels(29400);
-	for (int i = 0; i < trainData.size(); i++)
-	{
-		trainData[i] = mnist.images[i];
-		trainLabels[i] = mnist.labels[i];
-	}
-		
 	double T = 10;
 	for (int i = 0; i < 200000; i++)
 	{
-		myNet.doAnnealingStep(trainData, trainLabels, T);\
-		if (i%5==0)
-			T = 0.7 * T;
+		myNet.doAnnealingStep(mnist.images, mnist.labels, 29400, T); \
+			if (i % 5 == 0)
+				T = 0.7 * T;
 
 		int res = 0;
 		double error = 0;
@@ -102,7 +67,15 @@ int main()
 		cout << "Success rate: " << res / 12600.0 << endl;
 		cout << "Average error: " << error / 12600.0 << endl;
 	}
-	return 0;
+}
+
+void doSGDOnMnist(NeuralNetwork &myNet, MNIST &mnist)
+{
+	double alpha = 0.1;
+	//29400 levon, 12600
+	int curIterationNum = 0;
+	double lastPercentage = 0;
+
 	for (int i = 0; i < 1000000; i++)
 	{
 
@@ -134,8 +107,8 @@ int main()
 				error += myNet.getAverageError(mnist.images[j + 29400], mnist.labels[j + 29400], ErrorFunctions::LOG);
 			}
 			cout << "Epoch: " << i / 294 << endl;
-			cout << "Success rate: "<< res / 12600.0 << endl;
-			cout << "Average error: "<< error / 12600.0 << endl;
+			cout << "Success rate: " << res / 12600.0 << endl;
+			cout << "Average error: " << error / 12600.0 << endl;
 
 
 			if (lastPercentage < res / 12600.0)
@@ -145,7 +118,7 @@ int main()
 				fout.close();
 				lastPercentage = res / 12600.0;
 			}
-		
+
 		}
 
 		int epoch = i / 294;
@@ -156,9 +129,26 @@ int main()
 		myNet.updateWeights(alpha, 0.01);
 	}
 
+}
 
-	int t;
-	cin >> t;
+int main()
+{
+	MNIST mnist("./train.csv", 42000);
+
+	vector<int> shape{ 784 , 28, 10 };
+
+	NeuralNetwork myNet(shape, Activations::SIGMOID, Activations::SIGMOID, Regularizations::L2, true);
+
+	ofstream fout("network.txt");
+	fout << myNet.toString();
+	fout.close();
+
+	cout << fixed << setprecision(5);
+
+		
+	
+	doSGDOnMnist(myNet, mnist);
+	//doAnealingOnMnist(myNet, mnist);
 
 	return 0;
 }
